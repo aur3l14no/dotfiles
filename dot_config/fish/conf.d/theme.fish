273b664bb5e93797e9df9a5082a4d844e07d0782
aur3l14no <muzhi.yu1@gmail.com>
2026-02-03 15:26:28 +0800

    chezmoi/fish: migrate to 4.3

--- dot_config/fish/conf.d/theme.fish
+++ dot_config/fish/conf.d/theme.fish
@@ -1,39 +1,39 @@
 # solarized light
-set fish_color_autosuggestion 93a1a1
-set fish_color_cancel -r
-set fish_color_command 586e75
-set fish_color_comment 93a1a1
-set fish_color_cwd green
-set fish_color_cwd_root red
-set fish_color_end 268bd2
-set fish_color_error dc322f
-set fish_color_escape 00a6b2
-set fish_color_history_current --bold
-set fish_color_host normal
-set fish_color_host_remote yellow
-set fish_color_keyword 586e75
-set fish_color_match --background=brblue
-set fish_color_normal normal
-set fish_color_operator 00a6b2
-set fish_color_option 657b83
-set fish_color_param 657b83
-set fish_color_quote 839496
-set fish_color_redirection 6c71c4
-set fish_color_search_match bryellow --background=white
-set fish_color_selection white --bold --background=brblack
-set fish_color_status red
-set fish_color_user brgreen
-set fish_color_valid_path --underline
-set fish_pager_color_background
-set fish_pager_color_completion green
-set fish_pager_color_description B3A06D
-set fish_pager_color_prefix cyan --underline
-set fish_pager_color_progress brwhite --background=cyan
-set fish_pager_color_secondary_background
-set fish_pager_color_secondary_completion
-set fish_pager_color_secondary_description
-set fish_pager_color_secondary_prefix
-set fish_pager_color_selected_background --background=white
-set fish_pager_color_selected_completion
-set fish_pager_color_selected_description
-set fish_pager_color_selected_prefix
+set --global fish_color_autosuggestion 93a1a1
+set --global fish_color_cancel -r
+set --global fish_color_command 586e75
+set --global fish_color_comment 93a1a1
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end 268bd2
+set --global fish_color_error dc322f
+set --global fish_color_escape 00a6b2
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_keyword 586e75
+set --global fish_color_match --background=brblue
+set --global fish_color_normal normal
+set --global fish_color_operator 00a6b2
+set --global fish_color_option 657b83
+set --global fish_color_param 657b83
+set --global fish_color_quote 839496
+set --global fish_color_redirection 6c71c4
+set --global fish_color_search_match bryellow --background=white
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background
+set --global fish_pager_color_completion green
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix cyan --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background --background=white
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
